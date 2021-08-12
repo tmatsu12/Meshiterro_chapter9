e028cb5ff9a69f16a78252f52b2681c015153028
tmatsu12 <tmatsunaga65@gmail.com>
2021-08-12 05:22:13 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '4855aac0a67de91ceb33f89e5ca1d058dffd3b33f9cb3ca0cdb8c0d4d4c6647bd370e464ed08b9c2e224006c0b30f510c204b472ed0eeb3df24d0e0078c46926'
